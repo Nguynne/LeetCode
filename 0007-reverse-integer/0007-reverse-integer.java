13f3bc643077efa28b769493b69525d4d584767b
Nguynne <131970920+Nguynne@users.noreply.github.com>
2023-11-12 09:42:07 +0700

    Time: 1 ms (97.74%), Space: 40.1 MB (16.19%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,30 @@
 class Solution {
     public int reverse(int x) {
+        // Variable to store the reversed number
         long reverse = 0;
-        while(x != 0){
-            int remainer = x % 10;
+
+        // Reverse the digits of the number while it is not equal to 0
+        while (x != 0) {
+            // Get the last digit of the number
+            int remainder = x % 10;
+            
+            // Remove the last digit from the number
             x /= 10;
-            reverse = reverse * 10 + remainer;
+
+            // Build the reversed number by adding the last digit
+            reverse = reverse * 10 + remainder;
         }
-        if(x < 0)
+
+        // Check if the original number was negative and adjust the sign of the reversed number
+        if (x < 0)
             reverse *= -1;
-        if(reverse > Integer.MAX_VALUE || reverse<Integer.MIN_VALUE){
+
+        // Check for overflow or underflow, return 0 if out of the integer range
+        if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
             return 0;
         }
-        return (int)reverse;
+
+        // Cast the reversed number to int and return the result
+        return (int) reverse;
     }
 }
