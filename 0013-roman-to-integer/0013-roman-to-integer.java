90452713149b410877acd3281123c09dae468226
Nguynne <131970920+Nguynne@users.noreply.github.com>
2023-11-12 09:30:48 +0700

    Time: 5 ms (52.50%), Space: 43.7 MB (47.95%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,34 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
-        Map<String, Integer> roman = new HashMap<>();
-        roman.put("I", 1);
-        roman.put("V", 5);
-        roman.put("X", 10);
-        roman.put("L", 50);
-        roman.put("C", 100);
-        roman.put("D", 500);
-        roman.put("M", 1000);
-        int result = 0;
-
+        Map<Character, Integer> m = new HashMap<>();
+        
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        
+        int ans = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            // Corrected: Use charAt(i) to get the current character
-            String currentSymbol = String.valueOf(s.charAt(i));
-
-            // Corrected: Use get method to retrieve the value for the key
-            int value = roman.get(currentSymbol);
-
-            // Accumulate the value
-            result += value;
-
-            // Check for subtraction cases (e.g., IV, IX, etc.)
-            if (i > 0) {
-                String prevSymbol = String.valueOf(s.charAt(i - 1));
-                if (roman.get(prevSymbol) < value) {
-                    // Subtract twice the previous value
-                    result -= 2 * roman.get(prevSymbol);
-                }
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
             }
         }
-        return result;
+        
+        return ans;
     }
 }
