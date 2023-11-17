c0a4ea95491da3062ac507c982f45c605ac37887
Nguynne <131970920+Nguynne@users.noreply.github.com>
2023-11-17 20:00:37 +0700

    Time: 0 ms (100.00%), Space: 41.2 MB (6.86%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,13 +1,30 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder S = new StringBuilder(command.length());
-        for(int i = 0 ; i < command.length() ; i++){
-            if(command.charAt(i)=='G') S.append('G');
-            if(command.charAt(i)=='('){
-                if(command.charAt(i+1)==')') {S.append('o'); i++;}
-                else{S.append("al"); i = i + 2;}
+    // Create a StringBuilder to efficiently build and modify the resulting string
+    StringBuilder S = new StringBuilder(command.length());
+
+    // Iterate through each character in the input command
+    for (int i = 0; i < command.length(); i++) {
+        // Retain 'G' characters unchanged
+        if (command.charAt(i) == 'G') {
+            S.append('G');
+        }
+
+        // Check for the presence of '('
+        if (command.charAt(i) == '(') {
+            // If '(' is immediately followed by ')', replace with 'o' and skip the next character
+            if (command.charAt(i + 1) == ')') {
+                S.append('o');
+                i++;
+            } else {
+                // If '(' is followed by other character(s), replace with "al" and skip the next 3 characters
+                S.append("al");
+                i = i + 3;
             }
         }
-        return S.toString();
     }
+
+    // Convert the StringBuilder to a String and return the result
+    return S.toString();
+}
 }
