2a78a80bbcea42b4e60e39b84e358153223dfe80
NGUYEN HOANG NGUYEN <131970920+hoangnguyenn26@users.noreply.github.com>
2024-06-30 17:39:01 +0700

    Time: 21 ms (62.36%), Space: 27 MB (19.03%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cs
+++ 0278-first-bad-version/0278-first-bad-version.cs
@@ -3,10 +3,19 @@
 
 public class Solution : VersionControl {
     public int FirstBadVersion(int n) {
-        for (int i = n; i >= 0; i--){
-            if(IsBadVersion(i) == false)
-                return i + 1;
+        int start = 1;
+        int end = n;
+        while(start<end){
+        int mid = start+(end-start)/2;
+        Boolean isBad = IsBadVersion(mid);
+        
+        if(isBad == true){
+            end = mid;
         }
-        return 1;
+        else{
+            start = mid +1; 
+        }
+        }
+        return start;
     }
 }
