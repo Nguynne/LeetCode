7b365352296a013b5949021a4a6b227d9cf3f7a2
NGUYEN HOANG NGUYEN <131970920+Nguynne@users.noreply.github.com>
2024-06-30 16:40:26 +0700

    Time: 63 ms (78.63%), Space: 41.5 MB (21.67%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cs
+++ 0162-find-peak-element/0162-find-peak-element.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int FindPeakElement(int[] nums) {
         int max = nums[0];
-        for (int i = 0; i < nums.Length; i++){
+        for (int i = 1; i < nums.Length; i++){
             if (max < nums[i])
                 max = nums[i];
         }
