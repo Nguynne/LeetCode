1892b9a73b507d2eaae44dc7cfe7203a5e54ba44
NGUYEN HOANG NGUYEN <131970920+hoangnguyenn26@users.noreply.github.com>
2024-06-30 20:55:17 +0700

    Time: 27 ms (41.73%), Space: 26.9 MB (72.94%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cs
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int MySqrt(int x) {
-        return (int)Math.Sqrt(x);
+        if (x == 1 || x == 0)
+            return x;
+         int start = 1;
+        int end = x;
+        int mid = -1;
+
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if ((long) mid * mid > (long) x)
+                end = mid - 1;
+            else if (mid * mid == x)
+                return mid;
+            else
+                start = mid + 1;
+        }
+        return end;
     }
 }
