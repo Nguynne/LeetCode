d8e3d80bc47ecdb9e7d9ec6c613674790a7038be
NGUYEN HOANG NGUYEN <131970920+hoangnguyenn26@users.noreply.github.com>
2024-06-30 17:29:43 +0700

    Time: 1346 ms (5.42%), Space: 26.9 MB (39.58%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cs
@@ -0,0 +1,12 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        for (int i = n; i >= 0; i--){
+            if(IsBadVersion(i) == false)
+                return i + 1;
+        }
+        return 1;
+    }
+}
