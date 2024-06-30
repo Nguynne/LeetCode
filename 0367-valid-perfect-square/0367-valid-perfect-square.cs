dd202eedb4a46e18af8a1f9d98c8ce3de0e1f040
NGUYEN HOANG NGUYEN <131970920+Nguynne@users.noreply.github.com>
2024-06-30 16:11:17 +0700

    Time: 30 ms (64.94%), Space: 28.7 MB (68.53%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cs
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cs
@@ -1,9 +1,29 @@
-public class Solution {
-    public bool IsPerfectSquare(int num) {
-        for (int i = 1; i <= num/2 + 1; i++){
-            if (i * i == num)
+public class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+        int begin = 1;
+        int end = num;
+
+        while (begin <= end)
+        {
+            int mid = (begin + end) / 2;
+            long square = (long)mid * mid;
+
+            if (square == num)
+            {
                 return true;
+            }
+            else if (square < num)
+            {
+                begin = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
         }
+
         return false;
     }
 }
