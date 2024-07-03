0f02c323a1c2e39edb2a92fe9b49addbd75a4101
NGUYEN HOANG NGUYEN <131970920+hoangnguyenn26@users.noreply.github.com>
2024-07-03 15:17:03 +0700

    Time: 64 ms (48.05%), Space: 41.8 MB (55.47%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cs
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cs
@@ -3,9 +3,8 @@ public class Solution {
         int count = 0;
         
         for( int i = 0; i < nums.Length; i++){
-            if ( nums[i] % 3 != 0 && ((nums[i] + 1) % 3 == 0 || (nums[i] - 1) % 3 == 0)){
+            if ( nums[i] % 3 != 0 )
                 count++;
-            }
         }
         return count;
     }
